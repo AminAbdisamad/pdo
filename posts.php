4f84b37636b0e819ebcb38f945d8eb28a001ebfa
Amin Abdisamad <theaminux@gmail.com>
2018-04-15 18:10:27 +0300

    added positional param

--- posts.php
+++ posts.php
@@ -4,6 +4,14 @@ $stmt = $pdo->query('SELECT * FROM posts');
 // while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 //     echo $row['title'].'<br>';
 // }
-while($row = $stmt->fetch(PDO::FETCH_OBJ)){
+while($row = $stmt->fetch()){
     echo $row->title.'<br>';
 }
+
+//UNSAFE 
+$query = "SELECT * FROM posts WHERE author ='$author'";
+
+// Positional Parameter
+$author = 'Aminux';
+
+$sql = 'SELECT * FROM posts WHERE author = ?';
