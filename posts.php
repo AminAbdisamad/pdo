57d403346d1697e4d12fd27af4b1fea188904fd1
Amin Abdisamad <theaminux@gmail.com>
2018-04-15 18:21:02 +0300

    Added one row using propositional params

--- posts.php
+++ posts.php
@@ -5,13 +5,34 @@ $stmt = $pdo->query('SELECT * FROM posts');
 //     echo $row['title'].'<br>';
 // }
 while($row = $stmt->fetch()){
-    echo $row->title.'<br>';
+    //echo $row->title.'<br>';
 }
 
 //UNSAFE 
-$query = "SELECT * FROM posts WHERE author ='$author'";
+//$query = "SELECT * FROM posts WHERE author ='$author'";
 
 // Positional Parameter
+
+/* Insert Using Positional parameter */
+$title = 'Javascript Basics';
+$body = 'This is a javascript basics course that will cover the very foundation of js';
+$author = 'Aminux';
+$published = 'yes';
+
+
+
+
+
+/* SELECT FROM TABLE */
 $author = 'Aminux';
 
 $sql = 'SELECT * FROM posts WHERE author = ?';
+$prep = $pdo->prepare($sql);
+$prep->execute([$author]);
+$posts = $prep->fetchAll();
+
+foreach($posts as $post){
+    echo $post->title.'<br>';
+}
+
+
