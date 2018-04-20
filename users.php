3007d757de5669b564025a19055f5510c9f163ab
Amin Abdisamad <theaminux@gmail.com>
2018-04-20 23:06:05 +0300

    updated I dnk

--- users.php
+++ users.php
@@ -1,18 +1,9 @@
 <?php 
 require_once 'config/db.php';
 require_once 'config/config.php';
-// $username = username;
-// $email = email;
-// $status = status;
-
-// $sql = 'INSERT INTO users(username,email,status) VALUES (:username,:email,:status)';
-// $st = $pdo->prepare($sql);
-// $st->execute(['username'=>$username,'email'=>$emai,'status'=>$status]);
-
 /* Error Messages*/
 $msgColor = '';
 $msgText = '';
- 
 /* Insert Users to the database */
 if(isset($_POST['submit'])){
     $username = ($_POST['username']);
@@ -42,11 +33,22 @@ if(isset($_POST['delete'])){
   $sql = 'DELETE FROM users WHERE id = :id';
   $stmt = $pdo->prepare($sql);
   $stmt->execute(['id'=>$deleteId]);
-  $msg = "User Deleted successfully";
-  echo $msg;
- 
-
+  $msgColor = 'alert-success';
+  $msgText  = 'User Deleted Successfully';
+  header('location: '.URLROOT.'users.php');
 }
+
+/* Edit user */
+if(isset($_POST['edit'])){
+  $editId = $_POST['edit_id'];
+  $sql = 'UPDATE users SET () WHERE id = :id';
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(['id'=>$deleteId]);
+  $msgColor = 'alert-success';
+  $msgText  = 'User Deleted Successfully';
+  header('location: '.URLROOT.'users.php');
+}
+
 
 
 
@@ -94,8 +96,7 @@ if(isset($_POST['delete'])){
               <th>User Name</th>
               <th>Email</th>
               <th>Status</th>
-              <th>Options</th>
-              <th>Edit</th>
+              <th>Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -109,12 +110,6 @@ if(isset($_POST['delete'])){
               <input type="hidden" name="delete_id" value="<?php echo $post->id?>">
               <button class="btn-floating btn-medium waves-effect waves-light red" name="delete" type="submit"><i class="material-icons">delete</i></button>
             </form> 
-            </td>
-            <td>
-            <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-              <input type="hidden" name="edit_id" value="<?php echo $post->id?>">
-              <a class="btn-floating btn-medium waves-effect waves-light blue "><i class="material-icons">edit</i></a>
-            </form>
             </td>
           </tr>
         <?php endforeach; ?> 
